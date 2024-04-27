61b5a095a2de71ea8933dc1db77e6dd7be45c699
Jeffery <jefferyspippitt@gmail.com>
2024-04-26 20:37:44 -0500

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <section className="py-10">
+      <div className="container">
+        <h1 className="text-3xl font-bold">shadcn/ui Boilerplate</h1>
+      </div>
+    </section>
+  );
+}
---
+++ components/footer.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import {
+  FacebookLogo,
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  LinktreeLogo,
+  SnapchatLogo,
+  TiktokLogo,
+  TwitchLogo,
+  XLogo,
+} from "@phosphor-icons/react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="py-4">
+      <div className="container">
+        <p className="text-center text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+        </p>
+      </div>
+      <div className="flex justify-center mt-4">
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <GithubLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <XLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <FacebookLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <TwitchLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <TiktokLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <InstagramLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <LinkedinLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <SnapchatLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <LinkedinLogo size={24} />
+        </Link>
+        <Link
+          href="#"
+          rel="noopener noreferrer"
+          className="mx-2 text-gray-500 hover:text-gray-600"
+        >
+          <LinktreeLogo size={24} />
+        </Link>
+      </div>
+    </footer>
+  );
+}
