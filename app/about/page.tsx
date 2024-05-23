76de87317c238b45a6f049948480e3a624cd55ed
jefferypippitt <jefferyspippitt@gmail.com>
2024-05-23 00:30:36 -0500

    added active links

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
-export default function Home() {
+export default function About() {
   return (
     <section className="py-8">
       <div className="container">
         <h1 className="text-2xl font-bold">
-          Shadcn/ui <span className="text-blue-300">Boilerplate</span>
+          About <span className="text-green-300">Page</span>
         </h1>
       </div>
     </section>
--- components/header.tsx
+++ components/header.tsx
@@ -3,37 +3,40 @@
 import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
 
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  {
+    href: "/",
+    label: "home",
+  },
+  {
+    href: "/about",
+    label: "about",
+  },
+];
+
 export default function Header() {
+  const pathname = usePathname();
   return (
     <header className="py-4">
-      <nav className="container flex items-center justify-between">
-        <ul className="flex gap-6 items-center">
-          <li>
-            <Link
-              className="text-gray-500 transition-all hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-50"
-              href="/"
-            >
-              home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-gray-500 transition-all hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-50"
-              href="#"
-            >
-              about
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-gray-500 transition-all hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-50"
-              href="#"
-            >
-              contact
-            </Link>
-          </li>
+      <nav className="container flex justify-between items-center">
+        <ul className="flex gap-x-5 text-[14px]">
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <Link
+                className={`${
+                  pathname === link.href
+                    ? "text-black dark:text-neutral-200"
+                    : "text-neutral-500 dark:text-neutral-400"
+                }`}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
-
         <ThemeToggle />
       </nav>
     </header>
